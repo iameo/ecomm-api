8d8318adc13b9f14333759e804d8d6f081d46254
iameo <okwudike.emmanuel@gmail.com>
2022-04-03 12:51:42 +0100

    Product model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from accounts.models import ProductManager
+
 # Create your models here.
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200)
+    product_image = models.ImageField()
+    product_description = models.CharField(max_length=500)
+    product_quantity = models.IntegerField(default=1, blank=False)
+    product_ratings = models.FloatField(default=0.0)
+    product_shipping = models.CharField(max_length=200)
+    product_details = models.TextField(max_length=5000)
+    seller = models.ForeignKey(ProductManager, blank=False, null=False, related_name='seller_info', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product_name} - {self.seller} - {self.product_description[:20]}"
