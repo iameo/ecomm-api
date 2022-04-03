1aec56a8b88dbaad38ce13abcb748775ebc70255
iameo <okwudike.emmanuel@gmail.com>
2022-04-03 14:52:12 +0100

    admin setup

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ProductBuyer, ProductManager, CustomUser
 
 # Register your models here.
+
+@admin.register(ProductBuyer)
+class BuyerAdmin(admin.ModelAdmin):
+    readonly_fields = ('purchased_counts',)
+    date_hierarchy = 'acc_type__joined'
+
+@admin.register(ProductManager)
+class SellerAdmin(admin.ModelAdmin):
+    readonly_fields = ('availability', 'ratings', 'last_service', 'rendered_services')
+    date_hierarchy = 'acc_type__joined'
+
+admin.site.register(CustomUser)
