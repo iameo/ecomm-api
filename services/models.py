3c7072f5d4963b89a7b8ce2f757b22ce64ef95b5
iameo <okwudike.emmanuel@gmail.com>
2022-04-04 03:32:51 +0100

    added product item and order models --baseline

--- services/models.py
+++ services/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from accounts.models import ProductManager
-
+# from django.contrib.auth import get_user_model
+from django.conf import settings
 # Create your models here.
 
 
@@ -9,14 +9,38 @@ class Product(models.Model):
     """
     This is the Product Model which is populated by the Seller
     """
-    product_name = models.CharField(max_length=200)
-    product_image = models.ImageField()
-    product_description = models.CharField(max_length=500)
-    product_quantity = models.IntegerField(default=1, blank=False)
-    product_ratings = models.FloatField(default=0.0)
-    product_shipping = models.CharField(max_length=200)
-    product_details = models.TextField(max_length=5000)
-    seller = models.ForeignKey(ProductManager, blank=False, null=False, related_name='seller_info', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    image = models.ImageField()
+    description = models.CharField(max_length=500)
+    quantity = models.IntegerField(default=1, blank=False)
+    price = models.DecimalField(decimal_places=2, max_digits=15)
+    ratings = models.FloatField(default=0.0)
+    shipping = models.CharField(max_length=200)
+    details = models.TextField(max_length=5000)
+    # seller = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="products")
 
     def __str__(self):
-        return f"{self.product_name} - {self.seller} - {self.product_description[:20]}"
+        return f"{self.name} - {self.description[:20]} (Seller: {self.price})"
+
+    class Meta:
+        db_table = 'product_db'
+
+
+class ProductItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"ProductItem({self.quantity} x {self.product.name})"
+
+class ProductOrder(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    products = models.ManyToManyField(ProductItem)
+    reference_code = models.CharField(max_length=20, blank=True, null=True)
+    order_date = models.DateTimeField()
+    address = models.CharField(max_length=500)
+    ordered = models.BooleanField(default=False)
