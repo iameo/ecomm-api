9b43bfe096c373f1a5ef6b75ec1322d7885490cc
iameo <okwudike.emmanuel@gmail.com>
2022-04-04 03:33:34 +0100

    product admin updated to reflect model changes inclusion to admin page

--- services/admin.py
+++ services/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Product
+from .models import (Product, ProductItem, ProductOrder)
 
 # Register your models here.
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    readonly_fields = ('product_ratings',)
+    readonly_fields = ('ratings',)
+
+@admin.register(ProductItem)
+class ProductItemAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductOrder)
+class ProductOrderAdmin(admin.ModelAdmin):
+    pass
