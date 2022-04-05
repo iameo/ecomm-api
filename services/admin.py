a772c7da4b236f1ec2aa5a986c75aaf29bfc3a7d
iameo <okwudike.emmanuel@gmail.com>
2022-04-05 02:11:37 +0100

    services admin: base

--- services/admin.py
+++ services/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
-from .models import (Product, ProductItem, ProductOrder, ProductRating)
+from .models import (Product, ProductItem, ProductOrder, ProductRating, ProductImage)
 
 # Register your models here.
 
 
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ("name",)}
     readonly_fields = ('ratings',)
+    inlines = [ProductImageAdmin]
+
+    class Meta:
+        model = Product
 
 @admin.register(ProductItem)
 class ProductItemAdmin(admin.ModelAdmin):
@@ -18,4 +27,8 @@ class ProductOrderAdmin(admin.ModelAdmin):
 
 @admin.register(ProductRating)
 class ProductRatingAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
     pass
