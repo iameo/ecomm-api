3c3964f548eddcd755cf2b08ab14667b442f26b6
iameo <okwudike.emmanuel@gmail.com>
2022-04-06 18:10:11 +0100

    better url for product detail view

--- services/urls.py
+++ services/urls.py
@@ -7,6 +7,6 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductViewSet.as_view({'get':'list'}), name='home'),
     path('sellers/', views.SellerProductViewSet.as_view({'get':'list'}), name='homex'),
-    path('<int:pk>/', views.product_detail_view),
+    path('product/<int:pk>/', views.product_detail_view),
     path('product/', views.product_create_view),
 ]
