56b93ab1be434dd4c9e5af2caccd2a5d45221272
iameo <okwudike.emmanuel@gmail.com>
2022-04-09 15:45:39 +0100

    RegistrationSerialzer where Customer or Seller Serializer can inherit from and build their respective models + age validation (18 and above only, when creating accounts

--- accounts/auth_serializers.py
+++ accounts/auth_serializers.py
@@ -4,8 +4,10 @@ from .models import ProductManager, ProductBuyer, CustomUser
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.validators import UniqueValidator
 
+from utils import calculate_age
 
-class CustomerRegistrationSerializer(serializers.ModelSerializer):
+
+class RegistrationSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True,
         validators=[UniqueValidator(queryset=CustomUser.objects.all())]
@@ -13,36 +15,51 @@ class CustomerRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password]
     )
-    password2 = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'password', 'password2', 'first_name', 'last_name', 'dob')
+        fields = ['email', 'password', 'confirm_password', 'first_name', 'last_name', 'date_of_birth']
         extra_kwargs = {
             'first_name': {'required':True},
             'last_name': {'required':True},
-            'dob': {'required': True}
+            'date_of_birth': {'required': True}
         } 
         
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError("Passwords do not match")
-        return attrs
+        age = calculate_age(attrs['date_of_birth'])
 
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match")
+        if age < 18:
+            raise serializers.ValidationError("User must be at least 18 years old")
+        return attrs
+    
+
+class CustomerRegistrationSerializer(RegistrationSerializer):
     def create(self, validated_data):
         user = CustomUser.objects.create(
             email = validated_data['email'],
             first_name = validated_data['first_name'],
-            last_name = validated_data['last_name']
+            last_name = validated_data['last_name'],
+            date_of_birth = validated_data['date_of_birth'],
+            is_productBuyer = True,
         )
-        user.is_productBuyer = True
         user.set_password(validated_data['password'])
         user.save()
-        customer = ProductBuyer.objects.create(acc_type=user)
-        customer.save()
-        return customer
+        return user
 
 
-
-
+class SellerRegistrationSerializer(RegistrationSerializer):
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            email = validated_data['email'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+            date_of_birth = validated_data['date_of_birth'],
+            is_productManager = True,
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
