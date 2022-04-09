28d8ab3d80bf860fa84d5d6b83c1cc922e23133b
iameo <okwudike.emmanuel@gmail.com>
2022-04-10 00:06:15 +0100

    products/ now handled by createlist apiview

--- services/urls.py
+++ services/urls.py
@@ -1,12 +1,12 @@
 from unicodedata import name
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 
 urlpatterns = [
-    path('products/', views.ProductViewSet.as_view({'get':'list'}), name='home'),
+    # path('products/', views.ProductViewSet.as_view({'get':'list'}), name='home'),
     path('sellers/', views.SellerProductViewSet.as_view({'get':'list'}), name='homex'),
     path('product/<int:pk>/', views.product_detail_view),
-    path('product/', views.product_create_view),
+    path('products/', views.product_list_create_view),
 ]
