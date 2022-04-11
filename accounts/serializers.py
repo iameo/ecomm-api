46e277062330c8fa1bb1dffdaa96b4611681e2e4
iameo <okwudike.emmanuel@gmail.com>
2022-04-11 15:54:18 +0100

    RateSeller serializer included

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import ProductManager, ProductBuyer, SellerRating
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProductSellerSerializer(serializers.ModelSerializer):
@@ -15,7 +16,20 @@ class ProductBuyerSerializer(serializers.ModelSerializer):
         fields = ['acc_type', 'purchased_counts']
 
 
-class ProductSellerRatingSerializer(serializers.ModelSerializer):
+class RateSellerSerializer(serializers.ModelSerializer):
+    """Rate a Product Owner as a Customer"""
+    rate = serializers.FloatField(required=True)
     class Meta:
         model = SellerRating
         fields = ['seller', 'customer', 'rate']
+        extra_kwargs = {
+            'seller': {'required':True},
+            'customer': {'required':True},
+        } 
+    
+    def validate(self, attrs):
+        rate_ = attrs['rate']
+        if rate_ < 0 or rate_ > 5: #FloatField already makes sure null is not allowed
+            raise serializers.ValidationError("You can only rate a seller 0-5 stars")
+        return attrs
+
