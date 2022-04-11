5a215c58670f7b410213e0090792841636b6dd57
iameo <okwudike.emmanuel@gmail.com>
2022-04-11 15:23:41 +0100

    SellerAdmin update

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,13 +5,19 @@ from .models import ProductBuyer, ProductManager, CustomUser, SellerRating
 
 @admin.register(ProductBuyer)
 class BuyerAdmin(admin.ModelAdmin):
+    
     readonly_fields = ('purchased_counts',)
     date_hierarchy = 'acc_type__joined'
 
 @admin.register(ProductManager)
 class SellerAdmin(admin.ModelAdmin):
+    list_display = ('acc_type', 'ratings','availability','last_service', 'phone')
     readonly_fields = ('availability', 'ratings', 'last_service')
     date_hierarchy = 'acc_type__joined'
+
+    @admin.display(empty_value='')
+    def phone(self, obj):
+        return obj.acc_type.phone
 
 @admin.register(SellerRating)
 class SellerRatingAdmin(admin.ModelAdmin):
