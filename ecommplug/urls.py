da406330dfd3c342076e2316afb4c775069814e8
iameo <okwudike.emmanuel@gmail.com>
2022-04-12 17:29:47 +0100

    URLS updated

--- ecommplug/urls.py
+++ ecommplug/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/comments/', include('comments.urls')),
-    path('api/services/', include('services.urls'))
+    path('api/services/', include('services.urls')),
 ]
