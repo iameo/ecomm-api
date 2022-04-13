ba35b657e9b655338f440eaaa0ad054d19442ac2
iameo <okwudike.emmanuel@gmail.com>
2022-04-14 00:41:45 +0100

    Debug turned off

--- ecommplug/settings.py
+++ ecommplug/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', 'G0g3tyoUR53LfoN3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
