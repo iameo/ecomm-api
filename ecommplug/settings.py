1520cd603ce0be0c381a9f1d7e69e248d3094dd8
iameo <okwudike.emmanuel@gmail.com>
2022-04-14 00:50:53 +0100

    again with the whitenoise

--- ecommplug/settings.py
+++ ecommplug/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
